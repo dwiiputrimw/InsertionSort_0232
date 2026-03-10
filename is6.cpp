060dfe808abc8454d9b1303204f8dc7d54eebf6c
dwiiputrimw <dwiiputrimahriani@gmail.com>
2026-03-10 11:45:45 +0700

    Membuat fungsi InputData untuk memasukkan data

---
+++ is6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int arr [20];       // membuat array dengan panjang data 20
+int n;              // membuat variable inputan n
+
+void InputData (){ //procedur input
+    while (true){
+        cout << "MAsukkan Jumlah Data Pada Array : ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else {
+            cout << "\nArray Tidak Boleh Lebih Dari 20\n";
+        }
+    }
+}
+
+int main (){
+}
